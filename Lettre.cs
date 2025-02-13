8a466ef4ded641109f4d5d223ef21e7d2af35889
mou@inokscapital.com <MOU@inokscapital.com>
2025-02-13 15:28:08 +0100

    first commit

---
+++ Lettre.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Scrable
+{
+    public class Lettre  
+{  
+    public char Caractere { get; set; }  
+    public int Valeur { get; set; }  
+
+    public Lettre(char caractere, int valeur)  
+    {  
+        Caractere = caractere;  
+        Valeur = valeur;  
+    }  
+}
+}
